5cfab53d0dac31f2051622f627ffbf83493697a0
Saad el quaul <saadelquaul@gmail.com>
2025-09-24 10:00:11 +0100

    creating the Database config to setting up a PostgreSQL connection

---
+++ src/src/config/DbConfig.java
@@ -0,0 +1,9 @@
+package config;
+
+public class DbConfig {
+
+    public static final String URL = "jdbc:postgesql://localhost:5432/CryptoWalletSimulator";
+    public static final String USER = "postgres";
+    public static final String PASSWORD = "PASSWORD";
+
+}
